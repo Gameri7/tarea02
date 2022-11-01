5c664954132355dfcd7c141c1c2dd377996174ed
Gameri7 <114828587+Gameri7@users.noreply.github.com>
2022-11-01 01:53:10 -0500

    Add files via upload

---
+++ modeloscms1/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-hq0%9+h=*uqjjpks4(d2i0kede!6zz72^8c!#(-z_49h#do__b"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
